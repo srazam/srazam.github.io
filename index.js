62031c717fc15d126f25a9ee9354d931fa41773f
srazam <57676630+srazam@users.noreply.github.com>
2019-11-16 14:41:00 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+import "./styles.css";
+
+document.getElementById("app").innerHTML = `
+<h1>Hello Vanilla!</h1>
+<div>
+  We use Parcel to bundle this sandbox, you can find more info about Parcel
+  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
+</div>
+`;
+
+
+<iframe src="https://docs.google.com/forms/d/e/1FAIpQLSfGwvUC9Qw0iB_hyPZEELH__YCx8WpQWQjYn7ru-UVXye00Aw/viewform?embedded=true" width="640" height="1015" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
